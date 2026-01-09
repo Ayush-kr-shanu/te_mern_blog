331ef1354665a2552cfd71acf92a9e0e88a5deee
Ayush-kr-shanu <aksmilestone35@gmail.com>
2026-01-09 19:42:51 +0530

    removed logs

--- backend/src/service/token.service.js
+++ backend/src/service/token.service.js
@@ -1,7 +1,6 @@
 const { Token } = require("../models");
 
 const saveToken = async (tokenData) => {
-  console.log(tokenData,"==>>>>>>")
   const token = await Token.create(tokenData);
   return token
 };
